7b74cc26c76e5c2f95539e75744e909a54d0797e
ananijoel <ananijoellanatde@gmail.com>
2024-12-02 13:57:24 +0000

    mise a jour  de l'emploi du temps

--- Data-base/timetable.php
+++ Data-base/timetable.php
@@ -1,17 +1,17 @@
 <?php
-$subjects = [0 => "POO", 1 => "Equation differentielle", 2 => "Programation web", 3 => "Algebre", 4 => "CCNA", 5 => "anglais", 6 => "Pause", 7 => "Calcul integrale", 8 => "heure d'étude"];
+$subjects = [0 => "infographie", 1 => "Environnement des telecommunications", 2 => "Fondamentaux de base de donnees", 3 => "Outils et techniues de conception", 4 => "CCNA", 5 => "Notion de base sur la commutation et le routage", 6 => "Pause", 7 => "Statistiques", 8 => "heure d'étude"];
 
 $lundi = [
-    [$subjects[0], "730", "1030", "Mr. AMEVOR"],
-    ["Pause", "1030", "1100", "Pas de professeur"],
-    [$subjects[0], "1100", "1300", "MR. AMEVOR"],
+    [$subjects[8], "700", "1000", "Pas de professeur"],
+    ["Pause", "1000", "1030", "Pas de professeur"],
+    [$subjects[0], "1030", "1230", ""],
     ["Pause", "1300", "1400", "Pas de professeur"],
-    [$subjects[8], "1400", "1600", "Pas de professeur"],
-    [$subjects[8], "1600", "1800", "Pas de professeur"]
+    [$subjects[1], "1400", "1600", "Pas de professeur"],
+    [$subjects[1], "1600", "1800", "Pas de professeur"]
 ];
 
 $mardi = [
-    [$subjects[1], "730", "1030", "Dr PINDRA"],
+    [$subjects[8], "730", "1030", "Dr PINDRA"],
     ["Pause", "1030", "1100", "Pas de professeur"],
     [$subjects[2], "1100", "1300", "Mr. ATADEGNON"],
     ["Pause", "1300", "1400", "Pas de professeur"],
@@ -22,16 +22,16 @@ $mardi = [
 $mercredi = [
     [$subjects[4], "730", "1030", "Mr. AMUZUN"],
     ["Pause", "1030", "1100", "Pas de professeur"],
-    [$subjects[5], "1100", "1300", "Dr. MALDJA"],
+    [$subjects[8], "1100", "1300", "Dr. MALDJA"],
     ["Pause", "1300", "1400", "Pas de professeur"],
-    [$subjects[3], "1400", "1600", "Dr. NATA"],
-    [$subjects[8], "1600", "1800", "Pas de professeur"]
+    [$subjects[5], "1400", "1600", "Dr. NATA"],
+    [$subjects[3], "1600", "1800", "Pas de professeur"]
 ];
 
 $jeudi = [
-    [$subjects[0], "730", "1030", "Mr. Amevor"],
+    [$subjects[4], "730", "1030", "Mr. Amevor"],
     ["Pause", "1030", "1100", "Pas de professeur"],
-    [$subjects[4], "1100", "1300", "Mr. AMUZUN"],
+    [$subjects[2], "1100", "1300", "Mr. AMUZUN"],
     ["Pause", "1300", "1400", "Pas de professeur"],
     [$subjects[2], "1400", "1600", "Mr. ATADEGNON"],
     [$subjects[8], "1600", "1800", "Pas de professeur"]
@@ -40,10 +40,10 @@ $jeudi = [
 $vendredi = [
     [$subjects[7], "730", "1030", "Dr. PINDRA"],
     ["Pause", "1030", "1100", "Pas de professeur"],
-    [$subjects[2], "1100", "1300", "Mr. ATADEGNON"],
+    [$subjects[7], "1100", "1300", "Mr. ATADEGNON"],
     ["Pause", "1300", "1400", "Pas de professeur"],
-    [$subjects[7], "1400", "1600", "Dr. PINDRA"],
-    [$subjects[7], "1600", "1800", "Dr. PINDRA"]
+    [$subjects[8], "1400", "1600", "Dr. PINDRA"],
+    [$subjects[8], "1600", "1800", "Dr. PINDRA"]
 ];
 
 $time_table = ['Mon' => $lundi, 'Tue' => $mardi, 'Wed' => $mercredi, 'Thu' => $jeudi, 'Fri' => $vendredi];
